10940e0794026ad360a259ac28a874848503c2a5
Pranav Mailarpawar <hyprocus@gmail.com>
2025-03-10 19:59:13 +0530

    added GA

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,31 @@ import { lazy } from "react";
 import AchievementsShowcase from "./components/AchievementsShowcase";
 import Tech from "./components/Tech";
 
+const GoogleAnalytics = () => {
+  useEffect(() => {
+    if (!import.meta.env.VITE_GA_ID) {
+      console.log("issues with GA");
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${
+      import.meta.env.VITE_GA_ID
+    }`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", import.meta.env.VITE_GA_ID);
+  }, []);
+
+  return null;
+};
+
 
 // Lazy load components
 const Experience = lazy(() => import("./components/Experience"));
